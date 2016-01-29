f90fca4be65bc60055231e6f95598ec83ede75ef
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2016-01-29 23:12:00 +0200

    Add rugged gem; add repository discovery step to Differ; change return type to DifferResult (Struct) for Differ.get_changed_files

--- lib/upshop/differ.rb
+++ lib/upshop/differ.rb
@@ -1,24 +1,41 @@
 require "yaml"
+require "rugged"
 
 module Upshop
   module Differ
+    DifferResult = Struct.new(:status, :message, :files)
+    ERROR_STATUS = "error"
+    OK_STATUS = "ok"
+
     class << self
       def get_changed_files
+        result = DifferResult.new
         begin
+          discover_repository
           get_last_deployed_commit
           # if successful, run a diff and return a list of changed files
           # else return an explanation of why diff could not be calculated
         rescue DifferError => e
-          e.message
+          result.status = ERROR_STATUS
+          result.message = e.message
         end
+        result
       end
+
+      private
 
       def get_last_deployed_commit
         deploy_file = get_deploy_file
         extract_last_deployed_commit_from deploy_file
       end
 
-      private
+      def discover_repository
+        begin
+          @repo = Rugged::Repository.new("./")
+        rescue Rugged::RepositoryError
+          raise DifferError, "Unable to find git repository in current folder"
+        end
+      end
 
       def get_deploy_file
         begin
@@ -30,7 +47,8 @@ module Upshop
 
       def extract_last_deployed_commit_from(file)
         begin
-          file[:deploys][0].fetch(:commit)
+          @commit = file[:deploys][0].fetch(:commit)
+          raise(KeyError) unless @commit
         rescue KeyError, NoMethodError
           raise DifferError, "Unable to parse last deployed commit from deploy_file"
         end
--- spec/differ_spec.rb
+++ spec/differ_spec.rb
@@ -1,37 +1,64 @@
 require 'spec_helper'
 
+DIFFER = Upshop::Differ
+RESULT = Upshop::Differ::DifferResult
+
 RSpec.describe Upshop::Differ do
   describe "get_changed_files" do
-    context "when last deployed commit can be determined" do
-      context "when file diff can be determined" do
-        example do
 
+    describe "sad paths" do
+      describe "discover_repository" do
+        context "when repo is not discovered" do
+          example do
+            allow(Rugged::Repository).to receive(:new).and_raise Rugged::RepositoryError
+            expected_response = RESULT.new("error",
+                                           "Unable to find git repository in current folder")
+
+            expect(DIFFER.get_changed_files).to eq expected_response
+          end
         end
       end
 
-      context "when file diff cannot be determined" do
-        example do
+      describe "get_last_deployed_commit" do
+        before(:each) { allow(Rugged::Repository).to receive(:new) }
 
-        end
-      end
-    end
+        context "when file diff cannot be determined" do
+          example do
 
-    context "when last deployed commit cannot be determined" do
-      context "when deploy_file does not exist" do
-        example do
-          expected_response = "Unable to find deploy_file"
-          allow(YAML).to receive(:load_file).and_raise Errno::ENOENT
-
-          expect(Upshop::Differ.get_changed_files).to eq expected_response
+          end
         end
       end
 
-      context "when last deployed commit cannot be parsed from deploy_file" do
-        example do
-          expected_response = "Unable to parse last deployed commit from deploy_file"
-          allow(YAML).to receive(:load_file).and_return({ deploys: [] })
+      context "when last deployed commit cannot be determined" do
+        context "when deploy_file does not exist" do
+          example do
+            allow(YAML).to receive(:load_file).and_raise Errno::ENOENT
+            expected_response = RESULT.new("error",
+                                           "Unable to find deploy_file")
 
-          expect(Upshop::Differ.get_changed_files).to eq expected_response
+            expect(DIFFER.get_changed_files).to eq expected_response
+          end
+        end
+
+        context "when last deployed commit cannot be parsed from deploy_file" do
+          example do
+            allow(YAML).to receive(:load_file).and_return({ deploys: [] })
+            expected_response = RESULT.new("error",
+                                           "Unable to parse last deployed commit from deploy_file")
+
+            expect(DIFFER.get_changed_files).to eq expected_response
+          end
+        end
+
+        context "when last deployed commit is nil" do
+          example do
+            deploys = [{ commit: nil, date: "some date" }]
+            allow(YAML).to receive(:load_file).and_return({ deploys: deploys })
+            expected_response = RESULT.new("error",
+                                           "Unable to parse last deployed commit from deploy_file")
+
+            expect(DIFFER.get_changed_files).to eq expected_response
+          end
         end
       end
     end
